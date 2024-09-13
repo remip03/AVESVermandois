63e7f379d40a4dd37998524c5f5fc6d794a4d483
Rémi PLATEAU <remi.plateau@hotmail.fr>
2024-09-13 16:25:00 +0200

    travail sur la navbar

--- frontaves/src/app/commons/header/header.component.ts
+++ frontaves/src/app/commons/header/header.component.ts
@@ -1,20 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
-  dropDown = false;
+  dropDownACI = false;
+  dropDownServices = false;
 
-    // fonction pour ouvrir/fermer le menu déroulant
+    // fonction pour ouvrir/fermer le menu déroulant des ateliers chantiers d'insertion
   toggleACIDropDown() {
-    this.dropDown = !this.dropDown;
+    this.dropDownACI = !this.dropDownACI;
   }
+
+    // fonction pour ouvrir/fermer le menu déroulant des services
+  toggleServicesDropDown() {
+    this.dropDownServices = !this.dropDownServices;
+  }
+
+
 
 }
